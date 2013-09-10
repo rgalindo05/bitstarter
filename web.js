b9ecf2ae0fd1d5315278b4440a9993f5191205d1
rgalindo05 <ubuntu@ip-172-31-11-59.us-west-2.compute.internal>
2013-09-10 02:32:13 +0000

    added index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var outfile = "index.html";
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    fs.readFileSync(outfile);
 });
 
 var port = process.env.PORT || 5000;
