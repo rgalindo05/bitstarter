e6e62dd94dcd0b26357ab2aa046e5c24ee35652b
rgalindo05 <ubuntu@ip-172-31-11-59.us-west-2.compute.internal>
2013-09-10 04:19:53 +0000

    added read FileSycn

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var fs = require('fs');
-var outfile = "index.html";
+var htmfile = "index.html";
+ 
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-    fs.readFileSync(outfile);
+//    fs.readFileSync(outfile);
+    var html = fs.readFileSync(htmlfile).toString();
 });
 
 var port = process.env.PORT || 5000;
